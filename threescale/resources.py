eb09c1bc66e0403381f818a8d828d44b622d4243
Peter Stanko <peter.stanko0@gmail.com>
2019-03-05 14:34:22 +0100

    Proxy support and accounts

--- threescale/resources.py
+++ threescale/resources.py
@@ -1,7 +1,9 @@
 import logging
+from typing import Dict
 
-from threescale.defaults import DefaultClient, DefaultResource, DefaultPlanResource, \
-    DefaultUserResource
+from threescale import utils
+from threescale.defaults import DefaultClient, DefaultPlanResource, DefaultResource, \
+    DefaultStateClient, DefaultUserResource
 
 log = logging.getLogger(__name__)
 
@@ -46,7 +48,7 @@ class ApplicationPlans(DefaultClient):
         return self.parent.url + '/application_plans'
 
 
-class Accounts(DefaultClient):
+class Accounts(DefaultStateClient):
     def __init__(self, *args, entity_name='account', entity_collection='accounts', **kwargs):
         super().__init__(*args, entity_name=entity_name,
                          entity_collection=entity_collection, **kwargs)
@@ -56,14 +58,65 @@ class Accounts(DefaultClient):
         return self.threescale_client.admin_api_url + '/accounts'
 
     def create(self, params: dict = None, **kwargs) -> 'Account':
+        """Create new account
+        Args:
+            params(dict): Parameters to used to create new instance
+            **kwargs: Optional args
+        Returns(Account): Account instance
+        """
         return self.signup(params=params, **kwargs)
 
-    def signup(self, params, **kwargs) -> 'Account':
+    def signup(self, params: dict, **kwargs) -> 'Account':
+        """Sign Up for an account
+        Args:
+            params(dict): Parameters to used to create new instance
+            **kwargs: Optional args
+        Returns(Account): Account instance
+        """
         log.info(f"[SIGNUP] Create new Signup: {kwargs}")
         url = self.threescale_client.admin_api_url + '/signup'
         response = self.rest.post(url=url, json=params, **kwargs)
         instance = self._create_instance(response=response)
         return instance
+
+    def set_plan(self, entity_id: int, plan_id: int, **kwargs):
+        """Sets account plan for the account
+        Args:
+            entity_id: Entity id
+            plan_id: Plan id
+            **kwargs: Optional args
+        Returns:
+        """
+        log.info(f"[PLAN] Set plan for an account({entity_id}): {plan_id}")
+        params = dict(plan_id=plan_id)
+        url = self._entity_url(entity_id=entity_id) + '/change_plan'
+        response = self.rest.put(url=url, json=params, **kwargs)
+        instance = self._create_instance(response=response)
+        return instance
+
+    def send_message(self, entity_id: int, body: str, **kwargs) -> Dict:
+        """Send message to a developer account
+        Args:
+            entity_id(int): Entity id
+            body(str): Message body
+            **kwargs: Optional args
+        Returns(Dict): Response
+        """
+        log.info(f"[MSG] Send message to account ({entity_id}): {body} {kwargs}")
+        params = dict(body=body)
+        url = self._entity_url(entity_id=entity_id) + '/messages'
+        response = self.rest.post(url=url, json=params, **kwargs)
+        instance = utils.extract_response(response=response)
+        return instance
+
+    def approve(self, entity_id, **kwargs):
+        return self.set_state(entity_id=entity_id, state='approve', **kwargs)
+
+    def reject(self, entity_id, **kwargs):
+        return self.set_state(entity_id=entity_id, state='reject', **kwargs)
+
+    def pending(self, entity_id, **kwargs):
+        return self.set_state(entity_id=entity_id, state='make_pending', **kwargs)
 
 
 class Applications(DefaultClient):
@@ -108,7 +161,9 @@ class Tenants(DefaultClient):
 
 
 class Proxies(DefaultClient):
-    pass
+    def __init__(self, *args, entity_name='proxy', entity_collection='proxies', **kwargs):
+        super().__init__(*args, entity_name=entity_name,
+                         entity_collection=entity_collection, **kwargs)
 
 
 # Resources
@@ -165,6 +220,7 @@ class Account(DefaultResource):
     @property
     def applications(self) -> Applications:
         return Applications(parent=self, instance_klass=Application)
+
 
 class AccountUser(DefaultUserResource):
     @property
