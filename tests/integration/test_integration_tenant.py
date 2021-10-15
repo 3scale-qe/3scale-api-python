31ee89ed5ef99834df97203d9fa3c413e2e930ac
Alex Zgabur <azgabur@redhat.com>
2021-10-15 13:51:38 +0200

    billing added

--- tests/integration/test_integration_tenant.py
+++ tests/integration/test_integration_tenant.py
@@ -1,54 +1,36 @@
-from .asserts import assert_resource, assert_resource_params
 from datetime import date
+import backoff
 
-from ..test_3scale_api_client import _create_client
-
-
-def test_create_tenant(custom_tenant, tenant_params):
-    assert_resource(custom_tenant)
-    assert_resource_params(custom_tenant, tenant_params)
-
-
-def test_update_tenant(master_api, custom_tenant, tenant_params):
-    params = dict(from_email="anything@anything.invalid", support_email="anything2@anything.invalid",
-                  finance_support_email="anything3@anything.invalid", site_access_code="123456")
-    updated = master_api.tenants.update(custom_tenant.entity_id, params=params)
-    for key in params.keys():
-        assert updated[key] == params[key]
-
-
-def test_delete_tenant(master_api, tenant_params):
-    created = master_api.tenants.create(tenant_params)
-    created.delete()
-    deleted = master_api.tenants.read(created.entity_id)
-    assert deleted['state'] == "scheduled_for_deletion"
-
-
-def test_show_tenant(master_api, custom_tenant):
-    showed = master_api.tenants.read(custom_tenant.entity_id)
-    assert_resource(showed)
-    assert showed.entity_id == custom_tenant.entity_id
-
-
-def test_show_by_name_tenant(master_api, custom_tenant):
-    # because of lack of "tenant list" read_by_name doesnt work, after its added this can be uncommented
-
-    # showed = master_api.tenants.read_by_name(custom_tenant.entity_name)
-    # assert_resource(showed)
-    # assert showed.entity_id == custom_tenant.entity_id
-
-    pass
+from threescale_api import client
 
 
 def test_trigger_billing(master_api, custom_tenant):
-    returned = master_api.tenants.trigger_billing(custom_tenant, date.today().isoformat())
-    assert_resource(returned)
-    #todo
+    assert master_api.tenants.trigger_billing(custom_tenant, date.today().isoformat())
 
 
-def test_trigger_billing_account(master_api, custom_tenant):
-    #todo
-    tenant_api = _create_client(custom_tenant.url, custom_tenant['token'])
+def test_trigger_billing_account(master_api, custom_tenant, account_params):
+    admin_url = custom_tenant["signup"]["account"]["admin_base_url"]
+    token = custom_tenant["signup"]["access_token"]["value"]
+    tenant_api = client.ThreeScaleClient(admin_url, token, ssl_verify=False)
+    wait_for_tenant_initialise(tenant_api)
+
+    account = None
+    while not account:
+        accounts_list = tenant_api.accounts.list()
+        if len(accounts_list) >= 1:
+            account = tenant_api.accounts.list()[0].entity_id
+
+    assert master_api.tenants.trigger_billing_account(custom_tenant, account, date.today().isoformat())
 
 
-    returned = master_api.tenants.trigger_billing_account(custom_tenant)
+@backoff.on_predicate(backoff.fibo, lambda x: (not x.accounts.exists()) or len(x.accounts.list()) < 1 or
+                      (not x.account_plans.exists()) or len(x.account_plans.list()) < 1,
+                      max_tries=8, jitter=None)
+def wait_for_tenant_initialise(tenant):
+    """
+    Retries until an account plan is created. When that objects exists, tenant is ready.
+    When fetching account plans without previous check if they have been created
+    503 error can be returned
+    """
+    return tenant
+
