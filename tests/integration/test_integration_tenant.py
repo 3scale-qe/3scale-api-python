3eb5abebdc9ac5c7290ea51b003bb6d9acabbd49
Alex Zgabur <azgabur@redhat.com>
2021-11-19 14:35:47 +0100

    better_tests_tenant_billing

--- tests/integration/test_integration_tenant.py
+++ tests/integration/test_integration_tenant.py
@@ -1,21 +1,68 @@
-from datetime import date
+from datetime import date, timedelta
+from random import randint
+
+import backoff
+import pytest
 
 
-def test_trigger_billing(master_api, custom_tenant):
-    assert master_api.tenants.trigger_billing(custom_tenant, date.today().isoformat())
+# Some of the tests can fail randomly because of tenant not being 100% ready, be warned
+
+@pytest.fixture()
+def next_day() -> str:
+    return (date.today() + timedelta(days=1)).isoformat()
 
 
-def test_trigger_billing_resource(custom_tenant):
-    assert custom_tenant.trigger_billing(date.today().isoformat())
+@pytest.fixture()
+def paid_account(service_params, account_params, account_plans_params, application_plan_params):
+    def _paid_account(api):
+        """
+        Creates new account with payed app plan on default service.
+        No need for deletion as all is happening on temporary tenant.
+        """
+        service = api.services.list()[0]
+
+        account_params.update(org_name=f"test-{randint(1,10000)}", username=f"test-{randint(1,10000)}")
+        account = api.accounts.create(account_params)
+
+        application_plan_params.update(name=f"test-{randint(1,10000)}", setup_fee="5", cost_per_month="5")
+        app_plan = service.app_plans.create(params=application_plan_params)
+
+        application_params = dict(name=f"test-{randint(1,10000)}", description="desc", plan_id=app_plan.entity_id)
+        account.applications.create(params=application_params)
+
+        return account
+    return _paid_account
 
 
-def test_trigger_billing_account(master_api, custom_tenant):
-    # this test can sometimes fail randomly because of tenant not being 100% ready
-    account = custom_tenant.admin_api(wait=True).accounts.list()[0]
-    assert master_api.tenants.trigger_billing_account(custom_tenant, account, date.today().isoformat())
+@backoff.on_predicate(backoff.fibo, lambda x: x == 0, max_tries=8, jitter=None)
+def count_invoice(api, account):
+    # creating the invoice takes some time
+    return len(api.invoices.list_by_account(account))
 
 
-def test_trigger_billing_account_resource(custom_tenant):
-    # this test can sometimes fail randomly because of tenant not being 100% ready
-    account = custom_tenant.admin_api(wait=True).accounts.list()[0]
-    assert custom_tenant.trigger_billing_account(account, date.today().isoformat())
+def test_trigger_billing(master_api, custom_tenant, paid_account, next_day):
+    api = custom_tenant.admin_api(wait=True)
+    account = paid_account(api)
+    assert master_api.tenants.trigger_billing(custom_tenant, next_day)
+    assert count_invoice(api, account) == 1
+
+
+def test_trigger_billing_resource(custom_tenant, paid_account, next_day):
+    api = custom_tenant.admin_api(wait=True)
+    account = paid_account(api)
+    assert custom_tenant.trigger_billing(next_day)
+    assert count_invoice(api, account) == 1
+
+
+def test_trigger_billing_account(master_api, custom_tenant, paid_account, next_day):
+    api = custom_tenant.admin_api(wait=True)
+    account = paid_account(api)
+    assert master_api.tenants.trigger_billing_account(custom_tenant, account, next_day)
+    assert count_invoice(api, account) == 1
+
+
+def test_trigger_billing_account_resource(custom_tenant, paid_account, next_day):
+    api = custom_tenant.admin_api(wait=True)
+    account = paid_account(api)
+    assert custom_tenant.trigger_billing_account(account, next_day)
+    assert count_invoice(api, account) == 1
