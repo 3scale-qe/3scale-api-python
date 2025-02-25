f7ab3e6db1329f6dca451332baaa7d7a370d771d
Jose Manuel Castano <joscasta@redhat.com>
2025-02-25 11:45:04 +0100

    Remove empty last line.

--- tests/integration/test_integration_response_headers.py
+++ tests/integration/test_integration_response_headers.py
@@ -3,4 +3,3 @@ def test_x_served_by(api):
     response = api._rest.get(url=api.admin_api_url + '/accounts')
     assert response.status_code == 200
     assert "X-Served-By" not in response.headers
-    
