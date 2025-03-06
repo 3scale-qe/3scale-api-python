5e31794f2ad312af84fae1bd18cf60e712ad9e4b
Jose Manuel Castano <joscasta@redhat.com>
2025-03-06 11:10:59 +0100

    Simplify test for approve service subscription after PR review.

--- tests/integration/test_integration_service_subscriptions.py
+++ tests/integration/test_integration_service_subscriptions.py
@@ -26,6 +26,5 @@ def test_approve_service_subscription(account, service_subscription, service_pla
     asserts.assert_resource(service_subscription)
     asserts.assert_resource(account)
     asserts.assert_resource(service_plan)
-    svc_subs_id = service_subscription.entity_id
-    resource = account.service_subscriptions.approve(svc_subs_id)
+    resource = service_subscription.approve()
     asserts.assert_resource(resource)
